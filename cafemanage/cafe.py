2e23ad1bd6170b8b1a3a755781ccebd0a5d3f0a7
Theoneandonlynotorious <165252242+Theoneandonlynotorious@users.noreply.github.com>
2025-08-13 19:11:26 +0530

    Update cafe.py

--- cafemanage/cafe.py
+++ cafemanage/cafe.py
@@ -662,9 +662,10 @@ def main():
         else:
             st.warning("Only admin can access settings.")
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     if 'cart' not in st.session_state:
         st.session_state['cart'] = []
     main()
+
 
 
